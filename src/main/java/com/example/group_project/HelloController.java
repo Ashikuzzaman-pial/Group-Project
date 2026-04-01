7ffa0bb042ab57096441d028d4c36f7549bad560
oboni <2432013@iub.edu.bd>
2026-04-01 15:49:55 +0600

    hhhhhhh

--- src/main/java/com/example/group_project/HelloController.java
+++ src/main/java/com/example/group_project/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application Group Project oop course! ok");
+        welcomeText.setText("Welcome to JavaFX Application Group Project oop course spring ! ok");
         System.out.println("Github Update!");
         System.out.println("great");
     }
