d5015d5e0ef3bca5228ed5e07dd8b038b0d63e54
Ladly Khan <ladlykhan164@gmail.com>
2026-04-03 01:32:18 +0600

    hi

---
+++ src/main/java/com/example/group_project/AddReviewController.java
@@ -0,0 +1,24 @@
+package com.example.group_project;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AddReviewController
+{
+    @javafx.fxml.FXML
+    private ComboBox DesignCB;
+    @javafx.fxml.FXML
+    private TextField ApplicationIDTF;
+    @javafx.fxml.FXML
+    private TextArea RemarksTA;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitReviewButtonOA(ActionEvent actionEvent) {
+    }
+}
