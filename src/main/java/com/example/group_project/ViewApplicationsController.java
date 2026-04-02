dcd90a2dce567c77c6938bc38c0d92580a26c243
Ladly Khan <ladlykhan164@gmail.com>
2026-04-03 00:59:06 +0600

    hello

---
+++ src/main/java/com/example/group_project/ViewApplicationsController.java
@@ -0,0 +1,24 @@
+package com.example.group_project;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ViewApplicationsController
+{
+    @javafx.fxml.FXML
+    private Button ViewDetailsButtonOA;
+    @javafx.fxml.FXML
+    private TableColumn StatusTC;
+    @javafx.fxml.FXML
+    private TableView TableViewTV;
+    @javafx.fxml.FXML
+    private TableColumn EmployeeIDTC;
+    @javafx.fxml.FXML
+    private TableColumn ApplicationIdTC;
+    @javafx.fxml.FXML
+    private TableColumn CaseTypeTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
