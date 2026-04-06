239f0d5868b577263a7dd589493cc29ff0315bab
Ashikuzzaman-pial <ashikuzzamanpeal@gmail.com>
2026-04-06 12:02:14 +0600

    Pial

--- src/main/java/com/example/group_project/President/Controller/ApproveApplicationController.java
+++ src/main/java/com/example/group_project/President/Controller/ApproveApplicationController.java
@@ -1,6 +1,7 @@
 package com.example.group_project.President.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -10,12 +11,14 @@ public class ApproveApplicationController
     private TextField ApplicationIdTextFIeld;
     @javafx.fxml.FXML
     private TextArea RemarksTextArea;
+    @javafx.fxml.FXML
+    private DatePicker SubmissionDateFilterDP;
 
     @javafx.fxml.FXML
     public void initialize() {
     }
 
     @javafx.fxml.FXML
-    public void ApproveApplicationButtonOnAction(ActionEvent actionEvent) {
+    public void FilterApproveApplicationButtonOnAction(ActionEvent actionEvent) {
     }
 }
--- src/main/java/com/example/group_project/President/Controller/ViewApplicationsController.java
+++ src/main/java/com/example/group_project/President/Controller/ViewApplicationsController.java
@@ -1,27 +1,38 @@
 package com.example.group_project.President.Controller;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 
 public class ViewApplicationsController
 {
     @javafx.fxml.FXML
     private TableColumn StatusTC;
     @javafx.fxml.FXML
-    private TableColumn EmployeeIDTC;
-    @javafx.fxml.FXML
     private TableColumn ApplicationIdTC;
     @javafx.fxml.FXML
-    private TableColumn CaseTypeTC;
-    @javafx.fxml.FXML
     private TableView ViewApplicationTV;
+    @javafx.fxml.FXML
+    private DatePicker SubmissionDateDP;
+    @javafx.fxml.FXML
+    private ComboBox StatusCB;
+    @javafx.fxml.FXML
+    private TableColumn SubmissionDateTC;
+    @javafx.fxml.FXML
+    private TextField ApplicationIdTF;
+    @javafx.fxml.FXML
+    private TableColumn EmployeeNameTC;
+    @javafx.fxml.FXML
+    private TableColumn ApplicationTypeTC;
+    @javafx.fxml.FXML
+    private TextField EmployeeNameTF;
+    @javafx.fxml.FXML
+    private ComboBox ApplicationTypeCB;
 
     @javafx.fxml.FXML
     public void initialize() {
     }
 
     @javafx.fxml.FXML
-    public void ViewDetailsButtonOnAction(ActionEvent actionEvent) {
+    public void ViewDetailsButtonOA(ActionEvent actionEvent) {
     }
 }
