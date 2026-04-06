a46eaa2ed25b6d619a850fcbb0e90c7b3f4b6256
Ashikuzzaman-pial <ashikuzzamanpeal@gmail.com>
2026-04-07 01:03:07 +0600

    newFxml

---
+++ src/main/java/com/example/group_project/President/Controller/ApplicationFeedbackController.java
@@ -0,0 +1,40 @@
+package com.example.group_project.President.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class ApplicationFeedbackController
+{
+    @javafx.fxml.FXML
+    private TableView ApplicationFeedbackTV;
+    @javafx.fxml.FXML
+    private Label MessageLabel;
+    @javafx.fxml.FXML
+    private TextField SubjectTF;
+    @javafx.fxml.FXML
+    private TableColumn StatusTC;
+    @javafx.fxml.FXML
+    private Label SubjectLabel;
+    @javafx.fxml.FXML
+    private TableColumn DescriptionTC;
+    @javafx.fxml.FXML
+    private TextArea MessageTA;
+    @javafx.fxml.FXML
+    private TableColumn ApplicationIdTC;
+    @javafx.fxml.FXML
+    private TableColumn EmployeeNameTC;
+    @javafx.fxml.FXML
+    private TableColumn ApplicationTypeTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void SendButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RequestButtonOA(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/group_project/President/Controller/ViewApplicationDetailsController.java
+++
@@ -1,7 +0,0 @@
-package com.example.group_project.President.Controller;
-
-public class ViewApplicationDetailsController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
