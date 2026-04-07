19996cd17a449b5076ef141336663236557e22c4
Ladly Khan <ladlykhan164@gmail.com>
2026-04-08 00:00:38 +0600

    on the way

---
+++ src/main/java/com/example/group_project/FactoryRepresentative/FactoryRepresentativeDashboardController.java
@@ -0,0 +1,52 @@
+package com.example.group_project.FactoryRepresentative;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class FactoryRepresentativeDashboardController
+{
+    @javafx.fxml.FXML
+    private Label welcomeFactoryRepresentativeLabel;
+    @javafx.fxml.FXML
+    private Label factoryRepresentativeDashboardLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerFactoryButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void employeeListButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteFactoryButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateEmployeeButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registerEmployeeButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteEmployeeButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateFactoryButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void welfareIssueButtonOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/group_project/WelfareOfficerDashboardController.java
@@ -0,0 +1,52 @@
+package com.example.group_project;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class WelfareOfficerDashboardController
+{
+    @javafx.fxml.FXML
+    private Label welcomeWelfareOfficerLabel;
+    @javafx.fxml.FXML
+    private Label welfareOfficerDashboardLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updateStatusButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyEmployeeButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateApplicationStatusButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteReviewButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateReviewButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addReviewButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendNoteButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignedApplicationsButtonOA(ActionEvent actionEvent) {
+    }
+}
