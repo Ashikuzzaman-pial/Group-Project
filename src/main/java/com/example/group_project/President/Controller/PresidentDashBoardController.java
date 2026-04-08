eaa95041a1eedba697847c131a4963083e8185c8
Ashikuzzaman-pial <ashikuzzamanpeal@gmail.com>
2026-04-09 03:39:35 +0600

    Dashboard

---
+++ src/main/java/com/example/group_project/President/Controller/PresidentDashBoardController.java
@@ -0,0 +1,46 @@
+package com.example.group_project.President.Controller;
+
+import javafx.event.ActionEvent;
+
+public class PresidentDashBoardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void ViewReportButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewApplicationButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void applicationFeedbackButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateProgramButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteProgramButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectApplicationButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveApplicationButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addProgramButtonOA(ActionEvent actionEvent) {
+    }
+}
