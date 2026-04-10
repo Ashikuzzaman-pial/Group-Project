01b38382a7d04b5357f8fcf965085fc823db6842
Ladly Khan <ladlykhan164@gmail.com>
2026-04-10 13:19:51 +0600

    backworks

--- src/main/java/com/example/group_project/HelloApplication.java
+++ src/main/java/com/example/group_project/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/group_project/GeneralMember/GeneralMemberDashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/group_project/WelfareOfficer/WelfareOfficerDashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
